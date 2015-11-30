711202e9cd99d548648d2783800ec5091a832a1e
Pablo Carranza Velez <pablocarranza@gmail.com>
2015-11-30 11:20:59 -0800

    Check stuff before using it

--- Webcam-Face-Detect/webcam.py
+++ Webcam-Face-Detect/webcam.py
@@ -21,17 +21,18 @@ with open(pipeFile, 'w') as openPipe:
     while True:
         # Capture frame-by-frame
         ret, frame = video_capture.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        if ret && len(frame) > 0:
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        faces = faceCascade.detectMultiScale(
-            gray,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30, 30),
-            flags=cv2.cv.CV_HAAR_SCALE_IMAGE
-        )
-
-        openPipe.write(formatFaces(faces))
+            faces = faceCascade.detectMultiScale(
+                gray,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(30, 30),
+                flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+            )
+            if len(faces) > 0:
+                openPipe.write(formatFaces(faces))
 
 # When everything is done, release the capture
 video_capture.release()
