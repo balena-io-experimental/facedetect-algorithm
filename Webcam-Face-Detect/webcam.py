50ebb102bcdaec78f0f22490310eb1189e02736a
Pablo Carranza Velez <pablocarranza@gmail.com>
2015-11-30 20:03:34 -0800

    Try to open the pipe in a different way

--- Webcam-Face-Detect/webcam.py
+++ Webcam-Face-Detect/webcam.py
@@ -17,23 +17,23 @@ cascPath = sys.argv[1]
 faceCascade = cv2.CascadeClassifier(cascPath)
 
 video_capture = cv2.VideoCapture('/usr/src/FaceDetect/video.sdp')
-with open(pipeFile, 'w') as openPipe:
-    while True:
-        # Capture frame-by-frame
-        ret, frame = video_capture.read()
-        if ret and len(frame) > 0:
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+openPipe = open(pipeFile, 'w')
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+    if ret and len(frame) > 0:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-            faces = faceCascade.detectMultiScale(
-                gray,
-                scaleFactor=1.1,
-                minNeighbors=5,
-                minSize=(30, 30),
-                flags=cv2.cv.CV_HAAR_SCALE_IMAGE
-            )
-            if len(faces) > 0:
-                print(faces)
-                openPipe.write(formatFaces(faces))
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+        )
+        if len(faces) > 0:
+            print(faces)
+            openPipe.write(formatFaces(faces))
 
 # When everything is done, release the capture
 video_capture.release()
